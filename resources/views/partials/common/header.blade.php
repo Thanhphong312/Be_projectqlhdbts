a199f7846b4a0b4d5b97dc3c8a8da1c02578521d
thanhphong123 <57260920+Thanhphong312@users.noreply.github.com>
2023-05-23 14:10:46 +0700

    style button logout

--- resources/views/partials/common/header.blade.php
+++ resources/views/partials/common/header.blade.php
@@ -4,7 +4,7 @@
             <img style="height:75px; width: 100%;" src="http://haugiang.vnpt.vn/static/images/haugiang.png" alt="main_banner">
         </a>
         <div class="title-logo" >Hệ thống quản lý hợp đồng nhà trạm BTS</div>
-        <!-- <div class="d-flex justify-content-center align-items-bottom btn-logout">
+        <div class="d-flex justify-content-center align-items-bottom btn-logout">
             <a href=""> 
                 <div class="input-group form-rounded-1">
                     <input type="button" class="form-control" value="Đăng xuất">
@@ -12,7 +12,7 @@
                         <i class="fa fa-arrow-right"></i>
                     </span>
             </div>
-        </a> -->
+        </a>
         </div>
             <!-- top-banner.jpg -->
     </div>
