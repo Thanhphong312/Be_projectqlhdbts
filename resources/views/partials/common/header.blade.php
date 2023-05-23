68e84b0081ca4652184723b6cf714c438185cd85
thanhphong123 <57260920+Thanhphong312@users.noreply.github.com>
2023-05-23 14:54:22 +0700

    fix logout

--- resources/views/partials/common/header.blade.php
+++ resources/views/partials/common/header.blade.php
@@ -4,16 +4,6 @@
             <img style="height:75px; width: 100%;" src="http://haugiang.vnpt.vn/static/images/haugiang.png" alt="main_banner">
         </a>
         <div class="title-logo" >Hệ thống quản lý hợp đồng nhà trạm BTS</div>
-        <div class="d-flex justify-content-center align-items-bottom btn-logout">
-            <a href=""> 
-                <div class="input-group form-rounded-1">
-                    <input type="button" class="form-control" value="Đăng xuất">
-                    <span class="input-group-text">
-                        <i class="fa fa-arrow-right"></i>
-                    </span>
-            </div>
-        </a>
-        </div>
             <!-- top-banner.jpg -->
     </div>
 </header>
