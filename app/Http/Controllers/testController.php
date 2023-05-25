865b892381410d3707057682412a406cc4757bfa
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-25 15:33:44 +0700

    TTH - test

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+
 class testController extends Controller
 {
-    public function test(){
-       User::where(1)->nguoidungs();
+    public function test()
+    {
+        User::where(1)->nguoidungs();
     }
 }
