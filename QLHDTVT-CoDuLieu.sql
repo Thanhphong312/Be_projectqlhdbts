a00da08d9433e0a7332fa26cecd1d5f52738e61b
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-25 20:00:01 +0700

    TTH - csdl

---
+++ QLHDTVT-CoDuLieu.sql
@@ -0,0 +1,501 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2023 at 02:56 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qlhdtvt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `co_so_ha_tang`
+--
+
+CREATE TABLE `co_so_ha_tang` (
+  `CSHT_MaCSHT` varchar(255) NOT NULL,
+  `CSHT_TenCSHT` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `don_gia`
+--
+
+CREATE TABLE `don_gia` (
+  `DG_MaDon` varchar(255) NOT NULL,
+  `HD_MaHD` varchar(255) NOT NULL,
+  `DG_MaHD` varchar(255) NOT NULL,
+  `DG_Thang` varchar(255) NOT NULL,
+  `DG_Nam` varchar(255) NOT NULL,
+  `DG_Gia` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `don_vi`
+--
+
+CREATE TABLE `don_vi` (
+  `DV_MaDV` varchar(255) NOT NULL,
+  `DV_TenDV` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_hop_dong`
+--
+
+CREATE TABLE `file_hop_dong` (
+  `F_File` varchar(255) NOT NULL,
+  `HD_MaHD` varchar(255) NOT NULL,
+  `F_MaND` varchar(255) NOT NULL,
+  `F_Loai` varchar(255) NOT NULL,
+  `F_NgayTao` varchar(255) NOT NULL,
+  `F_NgaySua` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hop_dong`
+--
+
+CREATE TABLE `hop_dong` (
+  `HD_MaHD` varchar(255) NOT NULL,
+  `ND_MaND` bigint(20) UNSIGNED NOT NULL,
+  `T_MaTram` varchar(255) NOT NULL,
+  `DV_MaDV` varchar(255) NOT NULL,
+  `CSHT_MaCSHT` varchar(255) NOT NULL,
+  `HD_MaCSHT` varchar(255) NOT NULL,
+  `HD_MaDV` varchar(255) NOT NULL,
+  `HD_MaTram` varchar(255) NOT NULL,
+  `T_TenTram` varchar(255) NOT NULL,
+  `HD_NgayDangKy` varchar(255) NOT NULL,
+  `HD_NgayHetHan` varchar(255) NOT NULL,
+  `HD_NgayPhuLuc` varchar(255) NOT NULL,
+  `HD_GiaGoc` varchar(255) NOT NULL,
+  `HD_GiaHienTai` varchar(255) NOT NULL,
+  `HD_SoTaiKhoan` varchar(255) NOT NULL,
+  `HD_TenCTK` varchar(255) NOT NULL,
+  `HD_TenNH` varchar(255) NOT NULL,
+  `HD_TenChuDauTu` varchar(255) NOT NULL,
+  `HD_HDScan` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_05_24_150331_co_so_ha_tang', 1),
+(6, '2023_05_24_150415_quyen', 1),
+(7, '2023_05_24_150443_nguoi_dung', 1),
+(8, '2023_05_24_150501_don_vi', 1),
+(9, '2023_05_25_010415_nguoi_dung_don_vi', 1),
+(10, '2023_05_25_010740_quyen_nguoi_dung', 1),
+(11, '2023_05_25_025155_tram', 1),
+(12, '2023_05_25_025210_hop_dong', 1),
+(13, '2023_05_25_025248_don_gia', 1),
+(14, '2023_05_25_025312_file_hop_dong', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoi_dung`
+--
+
+CREATE TABLE `nguoi_dung` (
+  `ND_MaND` varchar(255) NOT NULL,
+  `ND_MaQ` varchar(255) NOT NULL,
+  `ND_MaDV` varchar(255) NOT NULL,
+  `ND_HoTen` varchar(255) NOT NULL,
+  `ND_GioiTinh` varchar(255) NOT NULL,
+  `ND_DiaChi` varchar(255) NOT NULL,
+  `ND_Email` varchar(255) NOT NULL,
+  `ND_MatKhau` varchar(255) NOT NULL,
+  `ND_SDT` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoi_dung_don_vi`
+--
+
+CREATE TABLE `nguoi_dung_don_vi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `ND_MaND` bigint(20) UNSIGNED NOT NULL,
+  `DV_MaDV` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quyen`
+--
+
+CREATE TABLE `quyen` (
+  `Q_MaQ` varchar(255) NOT NULL,
+  `Q_TenQ` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quyen_nguoi_dung`
+--
+
+CREATE TABLE `quyen_nguoi_dung` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `Q_MaQ` varchar(255) NOT NULL,
+  `ND_MaND` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tram`
+--
+
+CREATE TABLE `tram` (
+  `T_MaTram` varchar(255) NOT NULL,
+  `CSHT_MaCSHT` varchar(255) NOT NULL,
+  `T_MaCSHT` varchar(255) NOT NULL,
+  `T_TenTram` varchar(255) NOT NULL,
+  `T_DiaChi` varchar(255) NOT NULL,
+  `T_TinhTrang` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `ND_MaND` varchar(255) NOT NULL,
+  `ND_MaQ` varchar(255) NOT NULL,
+  `ND_MaDV` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `ND_GioiTinh` varchar(255) NOT NULL,
+  `ND_DiaChi` varchar(255) NOT NULL,
+  `ND_SDT` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `ND_MaND`, `ND_MaQ`, `ND_MaDV`, `name`, `ND_GioiTinh`, `ND_DiaChi`, `ND_SDT`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'ND_01', 'admin', '', 'admin', 'name', 'can tho', '123123', 'admin@gmail.com', NULL, '$2y$10$/BosiXfHxDHK.E.U7ULBlOkRAnCkCzTHpqnw.jr32xHbEBKP6a0pW', NULL, '2023-05-25 05:51:30', '2023-05-25 05:51:30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `co_so_ha_tang`
+--
+ALTER TABLE `co_so_ha_tang`
+  ADD PRIMARY KEY (`CSHT_MaCSHT`);
+
+--
+-- Indexes for table `don_gia`
+--
+ALTER TABLE `don_gia`
+  ADD PRIMARY KEY (`DG_MaDon`),
+  ADD KEY `don_gia_hd_mahd_foreign` (`HD_MaHD`);
+
+--
+-- Indexes for table `don_vi`
+--
+ALTER TABLE `don_vi`
+  ADD PRIMARY KEY (`DV_MaDV`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `file_hop_dong`
+--
+ALTER TABLE `file_hop_dong`
+  ADD PRIMARY KEY (`F_File`),
+  ADD KEY `file_hop_dong_hd_mahd_foreign` (`HD_MaHD`);
+
+--
+-- Indexes for table `hop_dong`
+--
+ALTER TABLE `hop_dong`
+  ADD PRIMARY KEY (`HD_MaHD`),
+  ADD KEY `hop_dong_nd_mand_foreign` (`ND_MaND`),
+  ADD KEY `hop_dong_t_matram_foreign` (`T_MaTram`),
+  ADD KEY `hop_dong_dv_madv_foreign` (`DV_MaDV`),
+  ADD KEY `hop_dong_csht_macsht_foreign` (`CSHT_MaCSHT`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nguoi_dung`
+--
+ALTER TABLE `nguoi_dung`
+  ADD PRIMARY KEY (`ND_MaND`),
+  ADD UNIQUE KEY `nguoi_dung_nd_email_unique` (`ND_Email`),
+  ADD UNIQUE KEY `nguoi_dung_nd_sdt_unique` (`ND_SDT`);
+
+--
+-- Indexes for table `nguoi_dung_don_vi`
+--
+ALTER TABLE `nguoi_dung_don_vi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nguoi_dung_don_vi_nd_mand_foreign` (`ND_MaND`),
+  ADD KEY `nguoi_dung_don_vi_dv_madv_foreign` (`DV_MaDV`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `quyen`
+--
+ALTER TABLE `quyen`
+  ADD PRIMARY KEY (`Q_MaQ`);
+
+--
+-- Indexes for table `quyen_nguoi_dung`
+--
+ALTER TABLE `quyen_nguoi_dung`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `quyen_nguoi_dung_q_maq_foreign` (`Q_MaQ`),
+  ADD KEY `quyen_nguoi_dung_nd_mand_foreign` (`ND_MaND`);
+
+--
+-- Indexes for table `tram`
+--
+ALTER TABLE `tram`
+  ADD PRIMARY KEY (`T_MaTram`),
+  ADD KEY `tram_csht_macsht_foreign` (`CSHT_MaCSHT`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_nd_sdt_unique` (`ND_SDT`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `nguoi_dung_don_vi`
+--
+ALTER TABLE `nguoi_dung_don_vi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quyen_nguoi_dung`
+--
+ALTER TABLE `quyen_nguoi_dung`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `don_gia`
+--
+ALTER TABLE `don_gia`
+  ADD CONSTRAINT `don_gia_hd_mahd_foreign` FOREIGN KEY (`HD_MaHD`) REFERENCES `hop_dong` (`HD_MaHD`);
+
+--
+-- Constraints for table `file_hop_dong`
+--
+ALTER TABLE `file_hop_dong`
+  ADD CONSTRAINT `file_hop_dong_hd_mahd_foreign` FOREIGN KEY (`HD_MaHD`) REFERENCES `hop_dong` (`HD_MaHD`);
+
+--
+-- Constraints for table `hop_dong`
+--
+ALTER TABLE `hop_dong`
+  ADD CONSTRAINT `hop_dong_csht_macsht_foreign` FOREIGN KEY (`CSHT_MaCSHT`) REFERENCES `co_so_ha_tang` (`CSHT_MaCSHT`),
+  ADD CONSTRAINT `hop_dong_dv_madv_foreign` FOREIGN KEY (`DV_MaDV`) REFERENCES `don_vi` (`DV_MaDV`),
+  ADD CONSTRAINT `hop_dong_nd_mand_foreign` FOREIGN KEY (`ND_MaND`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `hop_dong_t_matram_foreign` FOREIGN KEY (`T_MaTram`) REFERENCES `tram` (`T_MaTram`);
+
+--
+-- Constraints for table `nguoi_dung_don_vi`
+--
+ALTER TABLE `nguoi_dung_don_vi`
+  ADD CONSTRAINT `nguoi_dung_don_vi_dv_madv_foreign` FOREIGN KEY (`DV_MaDV`) REFERENCES `don_vi` (`DV_MaDV`),
+  ADD CONSTRAINT `nguoi_dung_don_vi_nd_mand_foreign` FOREIGN KEY (`ND_MaND`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `quyen_nguoi_dung`
+--
+ALTER TABLE `quyen_nguoi_dung`
+  ADD CONSTRAINT `quyen_nguoi_dung_nd_mand_foreign` FOREIGN KEY (`ND_MaND`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `quyen_nguoi_dung_q_maq_foreign` FOREIGN KEY (`Q_MaQ`) REFERENCES `quyen` (`Q_MaQ`);
+
+--
+-- Constraints for table `tram`
+--
+ALTER TABLE `tram`
+  ADD CONSTRAINT `tram_csht_macsht_foreign` FOREIGN KEY (`CSHT_MaCSHT`) REFERENCES `co_so_ha_tang` (`CSHT_MaCSHT`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
