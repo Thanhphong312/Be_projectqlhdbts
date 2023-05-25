9a12ef52a03e2d031523232456bbe0864b33396c
thanhphong123 <57260920+Thanhphong312@users.noreply.github.com>
2023-05-25 13:03:49 +0700

    fix seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\NguoiDung;
+use App\Models\User;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,14 +13,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        NguoiDung::create([
-            'ND_MaND'=>'ND_01',
-            'ND_HoTen'=>'admin',
-            'ND_GioiTinh'=>'Nam',
-            'ND_DiaChi'=>'Can Tho', 
-            'ND_Email'=>'admin@gmail.com', 
-            'ND_MatKhau'=>bcrypt('123123'), 
-            'ND_SDT'=>'123'
+        User::create([
+            'name'=>'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>'admin',
         ]);
     }
 }
