d389f8391ae8b6880423e81b46b44710b942d797
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-26 08:10:47 +0700

    TTH - change title Trang Chủ

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        $title = 'home';
+        $title = 'Trang Chủ';
         return view('home',compact('title'));
     }
 }
