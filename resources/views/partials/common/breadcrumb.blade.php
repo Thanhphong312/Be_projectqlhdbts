a4af5f59338bb95ef278608349abf647854d8f49
thanhphong123 <57260920+Thanhphong312@users.noreply.github.com>
2023-05-30 14:04:48 +0700

    slide bar

--- resources/views/partials/common/breadcrumb.blade.php
+++ resources/views/partials/common/breadcrumb.blade.php
@@ -1,5 +1,5 @@
 <ol class="breadcrumb ">
-    @if(strtolower($title)!='trang chủ')
+    @if(mb_strtolower($title)!='trang chủ')
         <li class="breadcrumb-item"><a href="{{route('home')}}">Trang Chủ</a></li>
         <li class="breadcrumb-item active"><a href="#">{{$title}}</a></li>
     @else
--- resources/views/partials/common/slide-bar.blade.php
+++ resources/views/partials/common/slide-bar.blade.php
@@ -1,21 +1,21 @@
 <nav id="sidebar" class="show-window hide-mobile">
     <ul class="list-unstyled components">
-        <li class="active">
+        <li class="{{mb_strtolower($title)=='trang chủ'?'active':''}}">
             <a href="{{route('home')}}">Trang Chủ</a>
         </li>
-        <li>
+        <li class="{{mb_strtolower($title)=='trạm'?'active':''}}">
             <a href="{{route('tram')}}">Trạm</a>
         </li>
-        <li>
+        <li class="{{mb_strtolower($title)=='hợp đồng'?'active':''}}">
             <a href="{{route('hopdong')}}">Hợp Đồng</a>
         </li>
-        <li>
+        <li class="{{(mb_strtolower($title)=='cơ sở hạ tầng')?'active':''}}">
             <a href="{{route('csht')}}">Cơ Sở Hạ Tầng</a>
         </li>
-        <li>
+        <li class="{{mb_strtolower($title)=='tài khoản'?'active':''}}">
             <a href="{{route('taikhoan')}}">Tài Khoản</a>
         </li>
-        <li>
+        <li class="{{mb_strtolower($title)=='thống kê'?'active':''}}">
             <a href="{{route('thongke')}}">Thống Kê</a>
         </li>
         <li>
