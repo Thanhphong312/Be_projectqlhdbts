7fc9509d619f294338370560bcdd72e91458414b
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-30 14:31:21 +0700

    TTH - xong giao diện csht

--- resources/views/csht.blade.php
+++ resources/views/csht.blade.php
@@ -43,9 +43,54 @@
             </nav>
 
             <!-- Content -->
-            
+            <!-- Add_CSHT-->
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button class="btn btn-success me-md-2 mt-1 mb-1" type="button">
+                    <i class="fas fa-plus"></i> Thêm</button>
+            </div>
+
+            <!-- Table_show_CSHT  -->
+            <table class="table table-bordered text-center">
+                <thead>
+                    <tr>
+                        <th scope="col-6 col-md-4">STT</th>
+                        <th scope="col-6 col-md-4">Mã CSHT</th>
+                        <th scope="col-6 col-md-4">Tên CSHT</th>
+                        <th scope="col-6 col-md-4">Tùy chỉnh</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>MH001</td>
+                        <td>Hậu Giang</td>
+                        <td>
+                            <button class="btn btn-primary me-md-3">
+                                <i class="fas fa-edit"></i> Sửa
+                            </button>
+                            <button class="btn btn-danger me-md-3">
+                                <i class="fas fa-trash-alt"></i> Xóa
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>MH002</td>
+                        <td>Hậu Giang</td>
+                        <td>
+                            <button class="btn btn-primary me-md-3">
+                                <i class="fas fa-edit"></i> Sửa
+                            </button>
+                            <button class="btn btn-danger me-md-3">
+                                <i class="fas fa-trash-alt"></i> Xóa
+                            </button>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
         </div>
-        
+
     </div>
     <!-- end body -->
 </div>
