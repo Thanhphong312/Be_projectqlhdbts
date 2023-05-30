595286c006336d84725f41a2cb578523aca1711b
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-30 13:11:39 +0700

    TTH - cấu trúc file

--- resources/views/taikhoan.blade.php
+++ resources/views/taikhoan.blade.php
@@ -12,17 +12,41 @@
     <!-- start search -->
     @include('partials.common.search')
     <!-- end search  -->
-    
-    <!--  start slide bar  -->
+
+    <!-- start body -->
     <div class="wrapper">
         <!-- Sidebar  -->
         @include('partials.common.slide-bar')
 
         <!-- Page Content  -->
-        
+        <div id="content">
+            <!-- Tieu de -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+                <div class="container-fluid justify-content-start">
+                    <button type="button" id="sidebarCollapse" class="btn btn-secondary m-2 hidden-mobile">
+                        <i class="fas fa-align-left"></i>
+                    </button>
+                    <!-- page show in mobile -->
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto m-2 hidden-window" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+                    <nav aria-label="breadcrumb " style="height:25px">
+                        <ol class="breadcrumb ">
+                            <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
+                            <li class="breadcrumb-item active"><a href="#">Tài khoản</a></li>
+                        </ol>
+                    </nav>
+                    <!-- menu in mobile -->
+                    @include('partials.common.mobile-menu')
+                </div>
+            </nav>
+
+            <!-- Content -->
+            
+        </div>
         
     </div>
-    <!-- end slide bar -->
+    <!-- end body -->
 </div>
 @endsection
 
