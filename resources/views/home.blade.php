8db8b101695a35f17f54e5424ebbfdce71aede29
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-30 11:42:25 +0700

    TTH - update 30

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,57 +22,8 @@
   <!--  start slide bar  -->
   <div class="wrapper">
     <!-- Sidebar  -->
-<<<<<<< HEAD
-    <nav id="sidebar">
-      <ul class="list-unstyled components">
-        <li class="active">
-          <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Trang chủ</a>
-        </li>
-        <li>
-          <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Trang</a>
-          <ul class="collapse list-unstyled" id="pageSubmenu">
-            <li>
-              <a href="hopdong">Hợp Đồng</a>
-            </li>
-            <li>
-              <a href="tram">Trạm</a>
-            </li>
-            <li>
-              <a href="cosohatang">Cơ Sở Hạ Tầng</a>
-            </li>
-            <li>
-              <a href="taikhoan">Tài Khoản</a>
-            </li>
-            <li>
-              <a href="thongke">Thống Kê</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">Hỗ trợ</a>
-        </li>
-        <li>
-          <a href="#">Liên hệ</a>
-        </li>
-        <li>
-          <a href="#">Giới thiệu</a>
-        </li>
-      </ul>
-      <div class="d-flex justify-content-end btn-logout m-3">
-        <a href="{{route('logout')}}">
-          <div class="input-group">
-            <input type="button" class="form-control btn-sm btn-light" value="Đăng xuất">
-            <span class="input-group-text">
-              <i class="fa fa-arrow-left"></i>
-            </span>
-          </div>
-        </a>
-      </div>
-    </nav>
-=======
     @include('partials.common.slide-bar')
 
->>>>>>> 3b80a13976da1ffe7754551dfa5d31c893561dd2
     <!-- Page Content  -->
     <div id="content">
       <nav class="navbar navbar-expand-lg navbar-light bg-primary">
