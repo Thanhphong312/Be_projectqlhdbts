8b1813dc474f142a74a8846332d935c4a283953a
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-30 14:09:09 +0700

    TTH - active mobile

--- resources/views/partials/common/mobile-menu.blade.php
+++ resources/views/partials/common/mobile-menu.blade.php
@@ -1,20 +1,20 @@
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
     <div class="hidden-window show-mobile">
         <ul class="nav navbar-nav ml-auto">
-            <li class="nav-item active">
+            <li class="nav-item {{mb_strtolower($title)=='trạm'?'active':''}}">
                 <a href="{{route('tram')}}" class="nav-link">Trạm</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item {{mb_strtolower($title)=='hợp đồng'?'active':''}}">
                 <a href="{{route('hopdong')}}" class="nav-link">Hợp Đồng</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item {{mb_strtolower($title)=='cơ sở hạ tầng'?'active':''}}">
                 <a href="{{route('csht')}}" class="nav-link">Cơ Sở Hạ Tầng</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item {{mb_strtolower($title)=='tài khoản'?'active':''}}">
                 <a href="{{route('taikhoan')}}" class="nav-link">Tài Khoản</a>
             </li>
-            <li class="nav-item">
-                <a href="{{route('thongke')}}" class="nav-link">Thống kê</a>
+            <li class="nav-item {{mb_strtolower($title)=='thống kê'?'active':''}}">
+                <a href="{{route('thongke')}}" class="nav-link">Thống Kê</a>
             </li>
         </ul>
     </div>
