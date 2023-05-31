5eea9574182252f543fa93a86251957f95a8a297
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-31 20:50:09 +0700

    TTH - sửa main logo

--- resources/views/partials/common/header.blade.php
+++ resources/views/partials/common/header.blade.php
@@ -1,7 +1,7 @@
 <header>
-    <div class="main-logo">
-        <a href="{{route('home')}}" style="height:75px; width: 100%;">
-            <img style="height:75px; width: 100%;" src="http://haugiang.vnpt.vn/static/images/haugiang.png" alt="main_banner">
+    <div class="main-logo" style="height:75px">
+        <a href="{{route('home')}}">
+            <img style="height:75px;" src="http://haugiang.vnpt.vn/static/images/haugiang.png" alt="main_banner">
         </a>
         <div class="title-logo" >Hệ thống quản lý hợp đồng nhà trạm BTS</div>
             <!-- top-banner.jpg -->
