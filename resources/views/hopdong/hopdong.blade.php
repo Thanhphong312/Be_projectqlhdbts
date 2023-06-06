953baf663bc2f7d4ba770e20c231200643c9ce39
ThangB1910143 <104030447+NguyenChienThangB1910143@users.noreply.github.com>
2023-06-06 10:33:15 +0700

    NCT scan

--- resources/views/hopdong/hopdong.blade.php
+++ resources/views/hopdong/hopdong.blade.php
@@ -73,7 +73,7 @@
                                     <td>{{$row->T_TenTram}}</td>
                                     <td>{{$row->HD_MaCSHT}}</td>
                                     <td>{{$row->HD_TenChuDauTu}}</td>
-                                    <td>{{$row->HD_HDScan}}</td>
+                                    <td><a href="{{$row->HD_HDScan}}">Hợp Đồng PDF</a></td>
                                     <td><input type="date" name="" id="" value={{$row->HD_NgayPhuLuc}}></td>
                                     <td>
                                         <a href="{{route('hopdong-capnhat')}}" class="btn btn-primary me-md-3">
