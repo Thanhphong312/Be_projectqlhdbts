65f7096193fee63c8516c16f8e688be61f98b441
thanhhoax701 <tth1610.vl@gmail.com>
2023-06-15 16:04:54 +0700

    format export hợp đồng

--- resources/views/HDexport.blade.php
+++ resources/views/HDexport.blade.php
@@ -1,29 +1,29 @@
-<table>
+<h1 style="text-align: center;">DANH SÁCH HỢP ĐỒNG</h1>
+
+<table style="border: 1;">
     <thead>
         <tr>
-            <th>Mã Hợp Đồng</th>
-            <th>Mã Người Dùng</th>
-            <th>Mã Trạm</th>
-            <th>Mã Đơn Vị</th>
-            <th>Mã CSHT</th>
-            <th>Tên Trạm</th>
-            <th>Ngày Đăng Ký</th>
-            <th>Ngày Hết Hạn</th>
-            <th>Ngày Phụ Lục</th>
-            <th>Giá Gốc</th>
-            <th>Giá Hiện Tại</th>
-            <th>Số tài khoản</th>
-            <th>Tên Chủ Tài Khoản</th>
-            <th>Tên Ngân Hàng</th>
-            <th>Tên Chủ Đầu Tư</th>
-            <th>Hợp Đồng</th>
+            <th style="font-weight: bold;">Mã Hợp Đồng</th>
+            <th style="font-weight: bold;">Mã Trạm</th>
+            <th style="font-weight: bold;">Mã Đơn Vị</th>
+            <th style="font-weight: bold;">Mã CSHT</th>
+            <th style="font-weight: bold;">Tên Trạm</th>
+            <th style="font-weight: bold;">Ngày Đăng Ký</th>
+            <th style="font-weight: bold;">Ngày Hết Hạn</th>
+            <th style="font-weight: bold;">Ngày Phụ Lục</th>
+            <th style="font-weight: bold;">Giá Gốc</th>
+            <th style="font-weight: bold;">Giá Hiện Tại</th>
+            <th style="font-weight: bold;">Số tài khoản</th>
+            <th style="font-weight: bold;">Tên Chủ Tài Khoản</th>
+            <th style="font-weight: bold;">Tên Ngân Hàng</th>
+            <th style="font-weight: bold;">Tên Chủ Đầu Tư</th>
+            <th style="font-weight: bold;">Hợp Đồng</th>
         </tr>
     </thead>
     <tbody>
         @foreach($HopDong as $HopDong)
         <tr>
             <td>{{ $HopDong->HD_MaHD }}</td>
-            <td>{{ $HopDong->ND_MaND }}</td>
             <td>{{ $HopDong->T_MaTram }}</td>
             <td>{{ $HopDong->DV_MaDV }}</td>
             <td>{{ $HopDong->HD_MaCSHT }}</td>
